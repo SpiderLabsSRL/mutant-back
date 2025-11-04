2db7ca8c26228bb2f825b43dca9287b0d9688ef0
CapRodri24 <201905170@est.umss.edu>
2025-11-04 13:00:48 -0400

    fix:s

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const allowedOrigins = [
   "http://localhost:8080",
   "https://mutantgym.netlify.app",//front en linea
   "https://mutant-back.onrender.com",
-  "https://mutant-back-r1pe.onrender.com", //back en linea
+  //"https://mutant-back-r1pe.onrender.com", //back en linea
 ];
 
 // Opciones de configuración CORS
